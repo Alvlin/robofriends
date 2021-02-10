f3b24d1b176a0948d53c3c7e2b03b149b1a2a03b
Alvin Lin <alvinlin425@gmail.com>
2021-02-10 02:51:19 -0500

    Automated Card additions

---
+++ src/robots.js
@@ -0,0 +1,53 @@
+export const robots = [
+{
+  id: 1,
+  name: 'name1',
+  username: 'user1',
+  email: 'email1',
+  img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/147640272_2868906666722115_87500765823290022_n.jpg?_nc_cat=111&ccb=2&_nc_sid=58c789&_nc_ohc=AZWxp9jyKcwAX9Sxq1C&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=b8b8ad7d846b3311892f62860cdc6c35&oe=6045B822'
+},
+{
+  id: 2,
+  name: 'name2',
+  username: 'user2',
+  email: 'email2',
+  img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/147143634_1395364460803964_3565485991352490484_n.jpg?_nc_cat=111&ccb=2&_nc_sid=58c789&_nc_ohc=FJSRpHbhL1MAX8N601q&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=e5dd2ae15e7c9491c780517a61e86484&oe=60478A2D'
+},
+{
+  id: 3,
+  name: 'name3',
+  username: 'user3',
+  email: 'email3',
+  img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/147662867_2519750348331042_1741192356283231267_n.jpg?_nc_cat=106&ccb=2&_nc_sid=58c789&_nc_ohc=5ILYnHm2CVYAX_eaNFT&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=16d25afbc094d9f40363b978d34cf5cd&oe=6046BFF6'
+},
+{
+  id: 4,
+  name: 'name4',
+  username: 'user4',
+  email: 'email4',
+  img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/126087096_913300079201621_5632662999070229278_n.jpg?_nc_cat=111&ccb=2&_nc_sid=58c789&_nc_ohc=S0tzVwj5v8kAX-0owlE&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=193da59d5003b68abcd8e303afea5005&oe=6046AD91'
+},
+{
+  id: 5,
+  name: 'name5',
+  username: 'user5',
+  email: 'email5',
+  img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/147518912_260574562139742_5209508940577966402_n.jpg?_nc_cat=103&ccb=2&_nc_sid=58c789&_nc_ohc=YScxY9k3fh4AX8XcobZ&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=bd35f262291c081344ac94c26151f554&oe=604649A3'
+},
+{
+  id: 6,
+  name: 'name6',
+  username: 'user6',
+  email: 'email6',
+  img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/129051306_307827020393723_887685743280783622_n.jpg?_nc_cat=104&ccb=2&_nc_sid=58c789&_nc_ohc=oyyC6N2uOIkAX9tkf3s&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=93479ee72374f4e5730c895f38fa0349&oe=6047D63C'
+},
+{
+  id: 7,
+  name: 'name7',
+  username: 'user7',
+  email: 'email7',
+  img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/142130246_3909757259074722_1494365691224369364_n.png?_nc_cat=108&ccb=2&_nc_sid=58c789&_nc_ohc=smrT1bXL77MAX_gLshC&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=d868d9d76d295dbfdcd3ff3f9526174d&oe=60398F9F'
+},
+
+
+]
