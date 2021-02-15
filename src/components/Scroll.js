b4e40c388df919d0ccf62de3999c00afd13b4118
Alvin Lin <alvinlin425@gmail.com>
2021-02-15 16:59:29 -0500

    Error Checking

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 class Scroll extends Component {
   render(){
     return(
-      <div style={{overflowY:'scroll', height:'760px', border:'solid 1px black'}}>
+      <div style={{overflowY:'scroll', height:'760px'}}>
         {this.props.children}
       </div>
     );
